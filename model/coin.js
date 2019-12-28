5db2b82e0ccd54e90f890c6b5d581c7c44550a4e
Ivan Kristianto <ivan.kristianto@gmail.com>
2019-12-29 01:02:09 +0700

    Use hardcoded coin saved model url

--- model/coin.js
+++ model/coin.js
@@ -1,7 +1,7 @@
 import * as autoMl from '@tensorflow/tfjs-automl';
 
 export async function loadModel(state) {
-    const modelUrl = 'http://localhost:1234/coin/model.json'; // URL to the model.json file.
+    const modelUrl = 'https://coin.ivankristianto.com/coin/model.json'; // URL to the model.json file.
     state.net = await autoMl.loadObjectDetection(modelUrl);
 }
 
